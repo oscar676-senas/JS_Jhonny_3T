9c97a293841d436e6001569b8ce45399493b4a99
Oscar Navarro <oscar676-senas@example.com>
2026-07-23 17:26:15 -0500

    Actualiza app.js y dependencias en package.json

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const express = require('express');  
+import express from "express"
+import "dotenv/config"
+
 const app = express();  
-const port = 3000; 
+const port = process.env.PORT || 3000; 
+
 app.get("/", (_, res) => {
      res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
 });  
