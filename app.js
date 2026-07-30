735077caefebefe747f2c8c5f8f418c64f09fb26
Oscar Andrés Navarro Ochoa <oscar.sena006@gmail.com>
2026-07-30 15:53:09 -0500

    cambios de los cuatro puntos realizados

--- app.js
+++ app.js
@@ -33,3 +33,93 @@ app.get("/productoss/:nombre", (req,res)=>{
 app.listen(port, function(){
     console.log(`Servidor funcionando ${port}`)
 })
+
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+/* ===========================
+1. Parámetro simple único
+=========================== */
+app.get("/saludo/:nombre", (req, res) => {
+    const { nombre } = req.params;
+
+    if (nombre.length < 3) {
+        return res.status(400).json({
+            error: "El nombre debe tener mínimo 3 letras."
+        });
+    }
+
+    res.send(`Hola ${nombre}, bienvenido`);
+});
+
+/* ===========================
+2. Parámetro simple único
+=========================== */
+app.get("/productos/:nombre", (req, res) => {
+    const { nombre } = req.params;
+
+    const producto = {
+        id: 1,
+        nombre: nombre,
+        stock: 30,
+        precioUnitario: 2500000,
+        categoria: "Tecnología"
+    };
+
+    res.json(producto);
+});
+
+/* ===========================
+3. Múltiples parámetros
+=========================== */
+app.get("/productos/:categoria/:id", (req, res) => {
+    const { categoria, id } = req.params;
+
+    res.json({
+        producto: id,
+        categoria: categoria,
+        servidor: "Servidor Express ADSO"
+    });
+});
+
+/* ===========================
+4. Params + Query
+=========================== */
+app.get("/usuarios/:id/posts", (req, res) => {
+
+    const { id } = req.params;
+    const { orden } = req.query;
+
+    let publicaciones = [
+        {
+            id: 1,
+            titulo: "Mi primera publicación"
+        },
+        {
+            id: 2,
+            titulo: "Aprendiendo Express"
+        },
+        {
+            id: 3,
+            titulo: "Uso de parámetros"
+        }
+    ];
+
+    if (orden === "desc") {
+        publicaciones.reverse();
+    }
+
+    res.json({
+        usuario: id,
+        orden: orden || "asc",
+        publicaciones
+    });
+
+});
+
+app.listen(port, () => {
+    console.log(`Servidor ejecutándose en el puerto ${port}`);
+});
