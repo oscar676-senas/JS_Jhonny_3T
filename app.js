55847c9fe9e454e52b4dd735c71d2a81f83b4f0e
williamestupinansilva-oss <williamestupinansilva@gmail.com>
2026-08-06 16:42:26 -0500

    Agrega endpoint de datos personales y del programa en formato JSON

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const express = require('express');  
-const app = express();  
-const port = 3000; 
-app.get("/", (_, res) => {
-     res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
-});  
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => { 
-    console.log( `Servidor en funcionamiento en el puerto: ${port} `); 
-}); 
+
+app.get("/", (_, res) => {
+    res.send("Hola, estamos aprendiendo express con la ficha 3407184");
+});
+
+
+app.get("/datos/personales", (_, res) => {
+    res.json({
+        nombre: "Oscar",
+        apellido: "Navarro",
+        telefono: "3123456789"
+    });
+});
+
+app.get("/datos/programa", (_, res) => {
+    res.json({
+        nombre: "Software",
+        programa: "ADSO",
+        ficha: "3407184"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+});
+
